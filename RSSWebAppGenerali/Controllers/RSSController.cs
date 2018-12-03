c5a0e36cd29be6974ba299e3c4e34614918184e2
benceAmbrus1 <o.g.bence@gmail.com>
2018-12-03 12:57:03 +0100

    First basic concept how RSS working , implement model service and controller for RSS feature also frontend view for it

--- RSSWebAppGenerali/Controllers/RSSController.cs
+++ RSSWebAppGenerali/Controllers/RSSController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RSSWebAppGenerali.Services;
 
 namespace RSSWebAppGenerali.Controllers
 {
@@ -11,7 +12,11 @@ namespace RSSWebAppGenerali.Controllers
         // GET: RSS
         public ActionResult Index()
         {
+            String url = "https://www.gamestar.hu/site/rss/rss.xml";
+            ViewBag.listRssItems = RSSService.Read(url);
             return View();
         }
     }
+
+
 }
---
+++ RSSWebAppGenerali/Services/RSSService.cs
@@ -0,0 +1,41 @@
+﻿using RSSWebAppGenerali.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.XPath;
+
+namespace RSSWebAppGenerali.Services
+{
+    public class RSSService
+    {
+        public static List<RSSItem> Read(string url)
+        {
+            List<RSSItem> listRssItems = new List<RSSItem>();
+            try
+            {
+                XPathDocument document = new XPathDocument(url);
+                XPathNavigator navigator = document.CreateNavigator();
+                XPathNodeIterator nodes = navigator.Select("//item");
+                while (nodes.MoveNext())
+                {
+                    XPathNavigator node = nodes.Current;
+                    listRssItems.Add(new RSSItem
+                    {
+                        Category = node.SelectSingleNode("category").Value,
+                        Description = node.SelectSingleNode("description").Value,
+                        Guid = node.SelectSingleNode("guid").Value,
+                        Link = node.SelectSingleNode("link").Value,
+                        PubDate = node.SelectSingleNode("pubDate").Value,
+                        Title = node.SelectSingleNode("title").Value
+                    });
+                }
+            }
+            catch
+            {
+                listRssItems = null;
+            }
+            return listRssItems;
+        }
+    }
+}
