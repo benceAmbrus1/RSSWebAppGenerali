8f866b06718fe3cd62ce9e3214bc711dc6c558e3
benceAmbrus1 <o.g.bence@gmail.com>
2018-12-03 12:07:03 +0100

    Basic of the site with auto generated authentication

--- RSSWebAppGenerali/App_Start/RouteConfig.cs
+++ RSSWebAppGenerali/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace RSSWebAppGenerali
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "RSS", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ RSSWebAppGenerali/Controllers/RSSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSSWebAppGenerali.Controllers
+{
+    public class RSSController : Controller
+    {
+        // GET: RSS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
